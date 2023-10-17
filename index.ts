17bbf1eb442dedf993329da014bf0b2a179ba9fa
rte76702 <rte76702@gmail.com>
2023-10-17 17:16:14 +0100

    initial commit

---
+++ index.ts
@@ -0,0 +1,7 @@
+import { reduce } from "simple-ts-lib";
+// how do i make it do the following:";
+// import { add } from "simple-ts-lib/utils";
+
+export const main = () => {
+  console.log('reduce("ADD", 0, 1,2,3): ', reduce("ADD", 0, 1, 2, 3));
+};
